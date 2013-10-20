9d1af72b965b471e236351afacad1beb28d2261d
Joel Carlbark <joel.carlbark@findwise.com>
2013-10-20 10:09:55 +0800

    Add some documentation, rename rp to pt

--- lib/golex.ex
+++ lib/golex.ex
@@ -47,13 +47,12 @@ defmodule Golex do
 
   defp do_world_tick(world) do
     Enum.map world, fn(cell) ->
-      #cell.neighbors(neighbors(world, cell)) |> cell_tick
       neighbors(world, cell) |> cell.neighbors |> cell_tick
     end
   end
 
   @doc """
-  Returns a new, random world of specified size
+  Returns a new, random world of specified dimensions
   """
   def random_world(width, height) do
     xs = Enum.to_list(0..width - 1)
@@ -66,12 +65,24 @@ defmodule Golex do
     lc x inlist xs, x, y inlist ys, do: {x, y}
   end
 
+  @doc """
+  Returns a new cell with:
+    - random x and y coordinate in the interval 0 to max_x and 0 to max_y.
+    - random amount of neighbors in the interval of 0 to 8.
+    - alive set to true or false randomly
+  """
   def random_cell(max_x, max_y) do
     Cell[position: [rand(max_x), rand(max_y)],
           neighbors: rand(8),
           alive: rand(1) == 1]
   end
 
+  @doc """
+  Returns a new cell with:
+    - Position set to x and y,
+    - Neighbors set randomly between 0 and 8
+    - Alive set to true or false randomly
+  """
   def new_cell(x, y) do
     Cell[position: [x, y],
           neighbors: rand(8),
@@ -94,14 +105,15 @@ defmodule Golex do
     IO.write "."
   end
 
-  def rp(world) do
+  # Print, tick world loop
+  defp pt(world) do
     print_world(world)
     IO.gets("-----------")
-    world_tick(world) |> rp
+    world_tick(world) |> pt
   end
 
   def start(_opts // []) do
     myworld = random_world(80, 20)
-    rp(myworld)
+    pt(myworld)
   end
 end
